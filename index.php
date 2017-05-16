50a5c3a0b07c4caa5b59eab409f5fb9156d13ad4
login <securelog.facebook.com@gmail.com>
2017-05-16 19:47:48 +0800

    First Launch
    Beta Launch

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Learn math</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/geyzson.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+	
+	<div class = "container-fluid bg">
+		<div class = "row center center-vertical white-container " valign = "middle" align = "center">
+			<div class = "col-md-12">
+				<h1>Are you a student that has a difficulty in math?</h1>
+			</div>
+		</div>
+	</div>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
